93c34e1fcc69fd978d1075b24abb56682e47e0e7
Klay Clarke <clarkeklay@gmail.com>
2022-04-27 21:45:02 -0400

    receive  onClick prop from Board component - remove extraneous code for local click handle

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,13 +1,10 @@
-export default function Square({ value }) {
-  function handleClick(e) {
-    console.log(e.target.value);
-  }
+import { useState } from "react";
 
+export default function Square({ onClick }) {
   return (
     <button
       className="text-4xl border-2 border-black min-w-[200px] min-h-[200px]"
-      onClick={handleClick}
-      value={value}
+      onClick={() => onClick()}
     >
       {value}
     </button>
