84dfa2463f58542ee4f6beaa6de22e4227b191cf
Klay Clarke <clarkeklay@gmail.com>
2022-04-27 21:43:02 -0400

    handle click on each game square from Board component (top down - allows us to easily figure out whether a person wins by referencing a global state at the top level of our app)

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from "react";
 import Square from "./Square";
 
-export default function Board({ value }) {
-  function renderSquare(i) {
-    return <Square value={i} />;
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const squaresClone = squares.slice();
+    squaresClone[i] = "X";
+    setSquares(squaresClone);
   }
+
+  function renderSquare(i) {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  }
+
   return (
     <div className="">
       <div className="grid grid-cols-3 border-2 border-black">
+        {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
         {renderSquare(3)}
@@ -15,7 +26,6 @@ export default function Board({ value }) {
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
-        {renderSquare(9)}
       </div>
     </div>
   );
