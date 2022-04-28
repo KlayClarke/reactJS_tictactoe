169464a3f1e2eeb4e14943c9367f19252ff68132
Klay Clarke <clarkeklay@gmail.com>
2022-04-27 20:21:25 -0400

    center gameboard by making Game component stretch across full viewport height (allows us to center components within it relative to user vh)

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,7 +2,7 @@ import Board from "./Board";
 
 export default function Game() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center h-screen">
       <Board />
     </div>
   );
