3d1b8c67996c887aa6e8807ae4ec9ecf619af177
Klay Clarke <clarkeklay@gmail.com>
2022-04-27 21:45:53 -0400

    receive current square's value from global array state on Board

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Square({ onClick }) {
+export default function Square({ value, onClick }) {
   return (
     <button
       className="text-4xl border-2 border-black min-w-[200px] min-h-[200px]"
