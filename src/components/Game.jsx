81a08e857d3dfd0823c186a1d2a7f142c2061cae
Klay Clarke <clarkeklay@gmail.com>
2022-04-28 16:29:54 -0400

    save each move in a history array and allow user to jump back to previous moves

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,22 +5,27 @@ export default function Game() {
   const [history, setHistory] = useState([
     {
       squares: Array(9).fill(null),
+      xIsNext: true,
     },
   ]);
-  // const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
 
   function handleSquareClick(i) {
     const current = history[history.length - 1];
     const squaresClone = current.squares.slice();
-    xIsNext ? (squaresClone[i] = "X") : (squaresClone[i] = "O");
-    setHistory(history.concat([{ squares: squaresClone }]));
-    setXIsNext(!xIsNext);
+    current.xIsNext ? (squaresClone[i] = "X") : (squaresClone[i] = "O");
+    setHistory(
+      history.concat([{ squares: squaresClone, xIsNext: !current.xIsNext }])
+    );
   }
 
   function handleRestart() {
-    setHistory([{ squares: Array(9).fill(null) }]);
-    setXIsNext(true);
+    setHistory([{ squares: Array(9).fill(null), xIsNext: true }]);
+  }
+
+  function jumpTo(index) {
+    // cut history array up to desired gameboard then render squares
+    const newHistory = history.slice();
+    setHistory(newHistory.splice(0, index));
   }
 
   function findWinner(squares) {
@@ -53,24 +58,47 @@ export default function Game() {
   const winner = findWinner(current.squares);
   const status = winner
     ? `The Winner Is ${winner}`
-    : `Next Player: ${xIsNext ? "X" : "O"}`;
+    : `Next Player: ${current.xIsNext ? "X" : "O"}`;
+
+  const moves = history.slice().map((step, move) => {
+    const desc = move ? "Jump to move #" + move : "Jump to game start";
+    return (
+      <li key={move}>
+        <button
+          className={`${winner ? "cursor-not-allowed" : ""} text-3xl `}
+          onClick={winner ? undefined : () => jumpTo(move + 1)}
+        >
+          {desc}
+        </button>
+      </li>
+    );
+  });
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-4xl mb-4">{status}</h1>
-      {winner && (
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
-          onClick={handleRestart}
-        >
-          Play Again
-        </button>
-      )}
-      <Board
-        squares={current.squares}
-        handleSquareClick={handleSquareClick}
-        winner={winner}
-      />
+    <div className="flex justify-center items-center gap-20">
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <h1 className="text-4xl mb-4">{status}</h1>
+        {winner && (
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+            onClick={handleRestart}
+          >
+            Play Again
+          </button>
+        )}
+        <Board
+          squares={current.squares}
+          handleSquareClick={handleSquareClick}
+          winner={winner}
+        />
+      </div>
+      <div
+        className={`${
+          winner ? "blur-sm" : ""
+        } border-2 border-black border-opacity-0 p-10 min-h-fit`}
+      >
+        <ul>{moves}</ul>
+      </div>
     </div>
   );
 }
