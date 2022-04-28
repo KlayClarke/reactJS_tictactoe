39e732a7954f2b70206dcb6fb780d8fad1c556a3
Klay Clarke <clarkeklay@gmail.com>
2022-04-27 23:06:04 -0400

    add function that checks for winner after each update

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Square from "./Square";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+  const [isGameOver, setIsGameOver] = useState(false);
 
   function handleClick(i) {
     const squaresClone = squares.slice();
@@ -16,7 +17,35 @@ export default function Board() {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   }
 
-  const status = `Next Player: ${xIsNext ? "X" : "O"}`;
+  function findWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        setIsGameOver(true);
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  const winner = findWinner(squares);
+  const status = winner
+    ? `The Winner Is ${winner}`
+    : `Next Player: ${xIsNext ? "X" : "O"}`;
 
   return (
     <div className="">
